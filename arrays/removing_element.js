ed2cda6b347a68b72a676ecf0c82ca46e5156aa1
Sirojiddin96 <herodot.sk@mail.ru>
2021-10-29 17:57:21 +0900

    removing is working, but duplicate number deleting is not working

---
+++ arrays/removing_element.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let index = nums.indexOf(val);
+  while (index !== -1) {
+    nums.splice(index, 1);
+    index = nums.indexOf(val);
+  }
+};
+
+removeElement([0, 0], 0);
